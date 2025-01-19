f10373256d744c784e25888cc75a744e09f28671
unknown <ivannabelichenko@gmail.com>
2025-01-19 11:57:22 +0200

    321

--- js/task-1.js
+++ js/task-1.js
@@ -5,6 +5,6 @@ liItem.forEach(el=>{
     let h2 = el.querySelector("h2")
     console.log("Category:",h2.textContent)
 
-    let ul = el.querySelectorAll("ul")
+    let ul = el.querySelectorAll("ul>li")
     console.log (`Elements: ${ul.length}`)
 });
